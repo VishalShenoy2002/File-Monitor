4719f63466854d2f251000f94fbf35e6f816e158
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-11-11 09:36:23 +0530

    Created main.py which bring all the files together to make it work

---
+++ main.py
@@ -0,0 +1,18 @@
+from secure_files import FileMonitor
+from utilities import *
+
+
+
+logo=getLogo()
+print(logo)
+
+# Loading Configuration
+config=loadConfig()
+print(config['name'])
+name=config['name']
+path=config['path']
+email=config['email']
+displayMessage(f"Welcome {name}.\nFile Monitor will be monitering {path}\nand we will notify you on {email} if we notice any changes. ")
+
+app=FileMonitor(path)
+app.run(config['interval'])
